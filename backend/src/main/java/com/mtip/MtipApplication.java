09ae44cdbb627ba9fd2242d4d3528ae75daf368e
dishanta-adhikari <dishantaadhikari.dev@gmail.com>
2026-05-03 01:26:48 +0530

    initial-commit

---
+++ backend/src/main/java/com/mtip/MtipApplication.java
@@ -0,0 +1,13 @@
+package com.mtip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtipApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mtip/MtipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
